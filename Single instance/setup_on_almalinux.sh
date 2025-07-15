ba860805ef90c620140a526d1c9c17a79dcf74cf
Petrov Evgeny <PetrovE2@ya.ru>
2025-07-15 14:07:45 +0300

    adding color text output functions

--- Single instance/setup_on_almalinux.sh
+++ Single instance/setup_on_almalinux.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+### Определение цветовых кодов ###
+ESC=$(printf '\033') RESET="${ESC}[0m" MAGENTA="${ESC}[35m" RED="${ESC}[31m" GREEN="${ESC}[32m"
+
+### Цветные функции ##
+magentaprint() { echo; printf "${MAGENTA}%s${RESET}\n" "$1"; }
+errorprint() { echo; printf "${RED}%s${RESET}\n" "$1"; }
+greenprint() { echo; printf "${GREEN}%s${RESET}\n" "$1"; }
+
 # Добавление репозитория MongoDB
+magentaprint "Добавление репозитория MongoDB"
 cat <<EOF > /etc/yum.repos.d/mongodb-org-6.0.repo
 [mongodb-org-6.0]
 name=MongoDB Repository
@@ -10,21 +19,32 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc
 EOF
 
+# Обновление пакетов
+magentaprint "Обновление пакетов ..."
 dnf update -y
 
 # Установка MongoDB
+magentaprint "Установка MongoDB ..."
 dnf install -y mongodb-org
 
 # Настройка, по какому IP будет доступна MongoDB
+magentaprint "Настройка IP для доступа к MongoDB"
 sed -i 's/bindIp: 127.0.0.1/bindIp: 127.0.0.1,10.100.10.1/' /etc/mongod.conf
-
-# Проверка статуса и вервии MongoDB:
-systemctl enable --now mongod
-systemctl status mongod --no-pager
-mongod --version
+grep -i "bindIp" /etc/mongod.conf
 
 # Настройка firewall:
+magentaprint "Настраиваем firewall для MongoDB:"
 firewall-cmd --permanent --add-port=27017/tcp
 firewall-cmd --reload
 firewall-cmd --list-all
+
+# Проверка статуса и вервии MongoDB:
+magentaprint "Проверка статуса MongoDB:"
+systemctl enable --now mongod
+systemctl status mongod --no-pager
+
+magentaprint "Проверка версии MongoDB:"
+mongod --version
+
+greenprint "MongoDB успешно установлен и настроен!"
 
